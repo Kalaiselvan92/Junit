814fbb055da5714b0783b0502128ef2701db720f
Kalaiselvan92 <58142988+Kalaiselvan92@users.noreply.github.com>
2022-05-11 12:57:03 +0530

    Create deployment.sh

---
+++ deployment.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+ART=$(curl https://ksdemo.jfrog.io/artifactory/api/storage/maven-demo/com/javainuse/SpringBootHelloWorld/0.0.1-SNAPSHOT/ | grep '.uri' | awk ' { print $3} ' | sed s/\"//g | sed s/,//g | tr -d '/' | grep jar | awk 'END{print}')
+
+cd /home/runner; wget --user admin --password Password@123  https://ksdemo.jfrog.io/artifactory/maven-demo/com/javainuse/SpringBootHelloWorld/0.0.1-SNAPSHOT/$ART; ls -lrt; 
+
+
+nohup java -jar /home/runner/$ART &
