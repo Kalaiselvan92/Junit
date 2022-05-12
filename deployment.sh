c6f28e01ca57bd8c85f721bfa858ba4f50174eda
Kalaiselvan92 <58142988+Kalaiselvan92@users.noreply.github.com>
2022-05-12 14:05:18 +0530

    Update deployment.sh

--- deployment.sh
+++ deployment.sh
@@ -3,7 +3,7 @@
 
 ART=$(curl https://ksdemo.jfrog.io/artifactory/api/storage/maven-demo/com/javainuse/SpringBootHelloWorld/0.0.1-SNAPSHOT/ | grep '.uri' | awk ' { print $3} ' | sed s/\"//g | sed s/,//g | tr -d '/' | grep jar | awk 'END{print}')
 
-cd /home/ec2-user; rm *.jar; wget --user admin --password Password@123  https://ksdemo.jfrog.io/artifactory/maven-demo/com/javainuse/SpringBootHelloWorld/0.0.1-SNAPSHOT/$ART; ls -lrt; 
+cd /home/runner; wget --user admin --password Password@123  https://ksdemo.jfrog.io/artifactory/maven-demo/com/javainuse/SpringBootHelloWorld/0.0.1-SNAPSHOT/$ART; ls -lrt; 
 
 
-java -jar /home/ec2-user/$ART 
+java -jar /home/runner/$ART 
