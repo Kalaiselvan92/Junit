5419e64608bc14242a9acc96900c0d45531eeef9
KALAI <kalaiselvan.m.92@gmail.com>
2022-05-25 17:33:10 +0530

    notification check

--- deployment.sh
+++ deployment.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+URL=curl https://ksdemo.jfrog.io/artifactory/api/storage/maven-demo/com/javainuse/SpringBootHelloWorld/1.1.1-SNAPSHOT/
 
-ART=$(curl https://ksdemo.jfrog.io/artifactory/api/storage/maven-demo/com/javainuse/SpringBootHelloWorld/1.1.1-SNAPSHOT/ | grep '.uri' | awk ' { print $3} ' | sed s/\"//g | sed s/,//g | tr -d '/' | grep jar | awk 'END{print}')
+ART=$($URL | grep '.uri' | awk ' { print $3} ' | sed s/\"//g | sed s/,//g | tr -d '/' | grep jar | awk 'END{print}')
 
-cd /home/runner; wget --user admin --password Password@123  https://ksdemo.jfrog.io/artifactory/maven-demo/com/javainuse/SpringBootHelloWorld/1.1.1-SNAPSHOT/$ART; ls -lrt; 
+cd /home/runner; wget --user admin --password Password@123  $URL/$ART; ls -lrt; 
 
 
 nohup java -jar /home/runner/$ART & 
