d0bc3fc3e5b4a124000dadc1e00219a3ee031ff8
Kalaiselvan92 <58142988+Kalaiselvan92@users.noreply.github.com>
2022-05-25 20:01:15 +0530

    Update deployment.sh

--- deployment.sh
+++ deployment.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-URL=curl https://ksdemo.jfrog.io/artifactory/api/storage/maven-demo/com/javainuse/SpringBootHelloWorld/1.1.1-SNAPSHOT/
+URL=https://ksdemo.jfrog.io/artifactory/api/storage/maven-demo/com/javainuse/SpringBootHelloWorld/1.1.1-SNAPSHOT
 
-ART=$($URL | grep '.uri' | awk ' { print $3} ' | sed s/\"//g | sed s/,//g | tr -d '/' | grep jar | awk 'END{print}')
+ART=$(curl $URL | grep '.uri' | awk ' { print $3} ' | sed s/\"//g | sed s/,//g | tr -d '/' | grep jar | awk 'END{print}')
 
 cd /home/runner; wget --user admin --password Password@123  $URL/$ART; ls -lrt; 
 
