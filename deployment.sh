fe42b8f653ece15b857ba46d4cf7944ed5d7441e
KALAI <kalaiselvan.m.92@gmail.com>
2022-05-26 13:15:18 +0530

    updated to old

--- deployment.sh
+++ deployment.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 
+
 URL=https://ksdemo.jfrog.io/artifactory/api/storage/springboot-libs-snapshot-local/com/javainuse/springboothelloWorld/0.0.1-SNAPSHOT
+
 
 
 ART=$(curl $URL | grep '.uri' | awk ' { print $3} ' | sed s/\"//g | sed s/,//g | tr -d '/' | grep jar | awk 'END{print}')
 
-cd /home/runner; curl -u admin:Password@123 -XGET  $URL/$ART --output springbootworld.jar; ls -lrt; 
+cd /home/runner; wget -N   $URL/$ART; mv $ART /home/runner/springbootworld.jar; ls -lrt; 
 
 
 
