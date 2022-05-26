7f95ca39247480e215f3df2456b3357901f31164
KALAI <kalaiselvan.m.92@gmail.com>
2022-05-26 11:12:01 +0530

    modified deployment file

--- deployment.sh
+++ deployment.sh
@@ -6,7 +6,8 @@ URL=https://ksdemo.jfrog.io/artifactory/api/storage/maven-demo/com/javainuse/Spr
 
 ART=$(curl $URL | grep '.uri' | awk ' { print $3} ' | sed s/\"//g | sed s/,//g | tr -d '/' | grep jar | awk 'END{print}')
 
-cd /home/runner; wget --user admin --password Password@123  $URL/$ART; ls -lrt; 
+cd /home/runner; wget --user admin --password Password@123  $URL/$ART; mv /home/runner/$ART /home/runner/springbootworld.jar; ls -lrt; 
 
 
-nohup java -jar /home/runner/$ART & 
+
+nohup java -jar /home/runner/springbootworld.jar & 
