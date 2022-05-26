3ff814509da06d83684005d113b807f1a3a1d4b9
KALAI <kalaiselvan.m.92@gmail.com>
2022-05-26 11:59:05 +0530

    removed login details in wget

--- deployment.sh
+++ deployment.sh
@@ -6,7 +6,7 @@ URL=https://ksdemo.jfrog.io/artifactory/api/storage/maven-demo/com/javainuse/Spr
 
 ART=$(curl $URL | grep '.uri' | awk ' { print $3} ' | sed s/\"//g | sed s/,//g | tr -d '/' | grep jar | awk 'END{print}')
 
-cd /home/runner; wget --user admin --password Password@123  $URL/$ART; mv /home/runner/$ART /home/runner/springbootworld.jar; ls -lrt; 
+cd /home/runner; wget -N  $URL/$ART; mv /home/runner/$ART /home/runner/springbootworld.jar; ls -lrt; 
 
 
 
