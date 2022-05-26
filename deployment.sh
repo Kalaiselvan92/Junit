8eae812bc464fffc1a4cede8a88f92053e3dd303
KALAI <kalaiselvan.m.92@gmail.com>
2022-05-26 11:18:29 +0530

    version changed to 0.1.1

--- deployment.sh
+++ deployment.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-URL=https://ksdemo.jfrog.io/artifactory/api/storage/maven-demo/com/javainuse/SpringBootHelloWorld/1.1.1-SNAPSHOT
+URL=https://ksdemo.jfrog.io/artifactory/api/storage/maven-demo/com/javainuse/SpringBootHelloWorld/0.1.1-SNAPSHOT
 
 
 ART=$(curl $URL | grep '.uri' | awk ' { print $3} ' | sed s/\"//g | sed s/,//g | tr -d '/' | grep jar | awk 'END{print}')
