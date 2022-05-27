07144d48f91d802910e1a46af4798750aa38e5f6
KALAI <kalaiselvan.m.92@gmail.com>
2022-05-27 13:42:00 +0530

    url updated

--- deployment.sh
+++ deployment.sh
@@ -8,7 +8,7 @@ URL=https://ksdemo.jfrog.io/artifactory/api/storage/default-maven-local/com/java
 
 ART=$(curl $URL | grep '.uri' | awk ' { print $3} ' | sed s/\"//g | sed s/,//g | tr -d '/' | grep jar | awk 'END{print}')
 
-cd /home/runner; wget -N  $URL/$ART; ls -lrt; 
+cd /home/runner; wget --user admin --password Password@123  https://ksdemo.jfrog.io/artifactory/default-maven-local/com/javainuse/SpringBootHelloWorld/0.0.1-SNAPSHOT/$ART; ls -lrt; 
 
 
 
