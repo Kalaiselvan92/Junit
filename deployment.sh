633dbbd6af213fb8f27c2eb8fb8f7d6bcf71422e
KALAI <kalaiselvan.m.92@gmail.com>
2022-05-27 11:42:30 +0530

    download jar file directly

--- deployment.sh
+++ deployment.sh
@@ -2,14 +2,14 @@
 
 
 
-URL=https://ksdemo.jfrog.io/artifactory/api/storage/springboot-libs-snapshot-local/com/javainuse/springboothelloWorld/0.0.1-SNAPSHOT
+#URL=https://ksdemo.jfrog.io/artifactory/api/storage/springboot-libs-snapshot-local/com/javainuse/springboothelloWorld/0.0.1-SNAPSHOT
+
+URL=https://ksdemo.jfrog.io/artifactory/springboot-libs-snapshot-local/com/javainuse/springboothelloWorld/0.0.1-SNAPSHOT/springboothelloWorld-0.0.1-20220526.105557-11.jar
+
+#ART=$(curl $URL | grep '.uri' | awk ' { print $3} ' | sed s/\"//g | sed s/,//g | tr -d '/' | grep jar | awk 'END{print}')
+
+cd /home/runner; wget -N --no-check-certificate  $URL; ls -lrt; 
 
 
 
-ART=$(curl $URL | grep '.uri' | awk ' { print $3} ' | sed s/\"//g | sed s/,//g | tr -d '/' | grep jar | awk 'END{print}')
-
-cd /home/runner; wget -N --no-check-certificate  $URL/$ART; mv $ART /home/runner/springbootworld.jar; ls -lrt; 
-
-
-
-nohup java -jar /home/runner/springbootworld.jar & 
+nohup java -jar /home/runner/springboothelloWorld-0.0.1-20220526.105557-11.jar & 
