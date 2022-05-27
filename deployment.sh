d9a7afdf0e75d4036ab74fe0faaeac99e08d0f0a
KALAI <kalaiselvan.m.92@gmail.com>
2022-05-27 13:03:35 +0530

    updated artifactory path

--- deployment.sh
+++ deployment.sh
@@ -2,14 +2,14 @@
 
 
 
-#URL=https://ksdemo.jfrog.io/artifactory/api/storage/springboot-libs-snapshot-local/com/javainuse/springboothelloWorld/0.0.1-SNAPSHOT
+URL=https://ksdemo.jfrog.io/artifactory/api/storage/default-maven-local/com/javainuse/SpringBootHelloWorld/0.0.1-SNAPSHOT
 
-URL=https://ksdemo.jfrog.io/artifactory/springboot-libs-snapshot-local/com/javainuse/springboothelloWorld/0.0.1-SNAPSHOT/springboothelloWorld-0.0.1-20220526.105557-11.jar
+
 
 #ART=$(curl $URL | grep '.uri' | awk ' { print $3} ' | sed s/\"//g | sed s/,//g | tr -d '/' | grep jar | awk 'END{print}')
 
-cd /home/runner; wget -N --no-check-certificate  $URL; ls -lrt; 
+cd /home/runner; wget -N  $URL/$ART; ls -lrt; 
 
 
 
-nohup java -jar /home/runner/springboothelloWorld-0.0.1-20220526.105557-11.jar & 
+nohup java -jar /home/runner/$ART & 
