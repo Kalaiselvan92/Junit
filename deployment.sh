54b0fb0a4491b5b5c43cabfabebeacc7e5a30cb8
KALAI <kalaiselvan.m.92@gmail.com>
2022-05-27 14:51:55 +0530

    updated jfrog path

--- deployment.sh
+++ deployment.sh
@@ -2,13 +2,13 @@
 
 
 
-URL=https://ksdemo.jfrog.io/artifactory/api/storage/default-maven-local/com/javainuse/SpringBootHelloWorld/0.0.1-SNAPSHOT
+URL=https://ksdemo.jfrog.io/artifactory/api/storage/junit-release/com/javainuse/SpringBootHelloWorld/0.0.1-SNAPSHOT
 
 
 
 ART=$(curl $URL | grep '.uri' | awk ' { print $3} ' | sed s/\"//g | sed s/,//g | tr -d '/' | grep jar | awk 'END{print}')
 
-cd /home/runner; wget --user admin --password Password@123  https://ksdemo.jfrog.io/artifactory/default-maven-local/com/javainuse/SpringBootHelloWorld/0.0.1-SNAPSHOT/$ART; ls -lrt; 
+cd /home/runner; wget --user admin --password Password@123  https://ksdemo.jfrog.io/artifactory/junit-release/com/javainuse/SpringBootHelloWorld/0.0.1-SNAPSHOT/$ART; ls -lrt; 
 
 
 
